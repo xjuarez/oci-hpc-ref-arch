b87f76ce515e381aba9c400ac5b5dbac2e15f89b
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-05-23 16:32:54 -0400

    Update mount_block.sh

--- mount_block.sh
+++ mount_block.sh
@@ -3,17 +3,16 @@
 export IQN=$1
 export IPV4=$2
 
-sudo iscsiadm -m node -o new -T $IQN -p $IPV4:3260
-sudo iscsiadm -m node -o update -T $IQN -n node.startup -v automatic
-sudo iscsiadm -m node -T $IQN -p $IPV4:3260 -l
+iscsiadm -m node -o new -T $IQN -p $IPV4:3260
+iscsiadm -m node -o update -T $IQN -n node.startup -v automatic
+iscsiadm -m node -T $IQN -p $IPV4:3260 -l
 
-sudo yum install parted
-sudo parted -l | grep Error
+yum install parted
+parted -l | grep Error
 lsblk
-sudo parted /dev/sdb mklabel gpt
-sudo parted -a opt /dev/sdb mkpart primary ext4 0% 100%
-sudo mkfs.ext4 -L datapartition /dev/sdb1
-sudo mkdir -p /mnt/blk
-sudo mount -o defaults /dev/sdb1 /mnt/blk
-sudo nano /etc/fstab
-/dev/sdb1 /mnt/blk ext4 defaults 0 2
+parted /dev/sdb mklabel gpt
+parted -a opt /dev/sdb mkpart primary ext4 0% 100%
+mkfs.ext4 -L datapartition /dev/sdb1
+mkdir -p /mnt/blk
+mount -o defaults /dev/sdb1 /mnt/blk
+echo "/dev/sdb1 /mnt/blk ext4 defaults 0 2" | tee -a /etc/fstab
