79bc7f9140b5e16a33ee861224dbdf9366ccdafc
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-05-23 08:08:38 -0400

    Update cli_deploy.sh

--- cli_deploy.sh
+++ cli_deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 #SET TENANCY
-export CNODES=4
+export USER=opc
+export CNODES=2
 export C=$1
 export PRE=`uuidgen | cut -c-5`
+export subnet=4
+
 export region=us-ashburn-1
 #export region=eu-frankfurt-1
+#export region=us-phoenix-1
+
 export AD=kWVD:US-ASHBURN-AD-1
 #export AD=kWVD:EU-FRANKFURT-1-AD-3
+#export AD=kWVD:PHX-AD-3
+
+#export OS=ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaf7f5z5qbj7y26ovgq3m6eqnhcgocrtzvjibhnbdyzi6rlgxp2koq #Ubuntu, FRA
 #export OS=ocid1.image.oc1.phx.aaaaaaaav4gjc4l232wx5g5drypbuiu375lemgdgnc7zg2wrdfmmtbtyrc5q #OracleLinux, PHX
 export OS=ocid1.image.oc1.iad.aaaaaaaautkmgjebjmwym5i6lvlpqfzlzagvg5szedggdrbp6rcjcso3e4kq #OracleLinux, IAD
+#export OS=ocid1.image.oc1.iad.aaaaaaaaafmyat7i5s7ae3aylwtvmt7v4dw4yy2thgzaqlbjzoxngrjg54xq
 #export OS=ocid1.image.oc1.eu-frankfurt-1.aaaaaaaamueig267buvha27g2zr7hqthtix55dnsc3yizwj62yxxjg4lwtka #OracleLinux, FRA
 #wget https://raw.githubusercontent.com/tanewill/oci_hpc/master/bm_configure.sh
 
@@ -18,29 +27,29 @@ export OS=ocid1.image.oc1.iad.aaaaaaaautkmgjebjmwym5i6lvlpqfzlzagvg5szedggdrbp6r
 #CREATE NETWORK
 echo
 echo 'Creating Network'
-V=`oci network vcn create --region $region --cidr-block 10.0.0.0/24 --compartment-id $C --display-name "hpc_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+V=`oci network vcn create --region $region --cidr-block 10.0.$subnet.0/24 --compartment-id $C --display-name "hpc_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
 NG=`oci network internet-gateway create --region $region -c $C --vcn-id $V --is-enabled TRUE --display-name "hpc_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
 RT=`oci network route-table create --region $region -c $C --vcn-id $V --display-name "hpc_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
 SL=`oci network security-list create --region $region -c $C --vcn-id $V --display-name "hpc_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
-S=`oci network subnet create -c $C --vcn-id $V --region $region --availability-domain "$AD" --display-name "hpc_subnet-$PRE" --cidr-block "10.0.0.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
+S=`oci network subnet create -c $C --vcn-id $V --region $region --availability-domain "$AD" --display-name "hpc_subnet-$PRE" --cidr-block "10.0.$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
 
 #CREATE FILE SYSTEM
-echo
-echo 'Creating File System'
-FSS=`oci fs file-system create --region $region --availability-domain "$AD" -c $C --display-name "HPC_File_System" --wait-for-state ACTIVE | jq -r '.data.id'`
-MT=`oci fs mount-target create --region $region --availability-domain "$AD" -c $C --subnet-id $S --display-name "mountTarget$PRE" --wait-for-state ACTIVE --ip-address 10.0.0.20 | jq -r '.data.id'`
+#echo
+#echo 'Creating File System'
+#FSS=`oci fs file-system create --region $region --availability-domain "$AD" -c $C --display-name "HPC_File_System" --wait-for-state ACTIVE | jq -r '.data.id'`
+#MT=`oci fs mount-target create --region $region --availability-domain "$AD" -c $C --subnet-id $S --display-name "mountTarget$PRE" --wait-for-state ACTIVE --ip-address 10.0.0.20 | jq -r '.data.id'`
 
 #CREATE HEADNODE
 echo
 echo 'Creating Headnode'
-masterID=`oci compute instance launch --region $region --availability-domain "$AD" -c $C --shape "BM.DenseIO2.52" --display-name "hpc_master-$PRE" --image-id $OS --subnet-id $S --private-ip 10.0.0.2 --wait-for-state RUNNING --user-data-file hn_configure.sh --ssh-authorized-keys-file ~/.ssh/id_rsa.pub | jq -r '.data.id'`
+masterID=`oci compute instance launch --region $region --availability-domain "$AD" -c $C --shape "BM.Standard1.36" --display-name "hpc_master-$PRE" --image-id $OS --subnet-id $S --private-ip 10.0.$subnet.2 --wait-for-state RUNNING --user-data-file hn_configure.sh --ssh-authorized-keys-file ~/.ssh/id_rsa.pub | jq -r '.data.id'`
 
 #CREATE COMPUTE
 echo
 echo 'Creating Compute Nodes'
-computeData=$(for i in `seq 1 $CNODES`; do oci compute instance launch --region $region --availability-domain "$AD" -c $C --shape "BM.Standard2.52" --display-name "hpc_cn$i-$PRE" --image-id $OS --subnet-id $S --assign-public-ip true  --user-data-file hn_configure.sh --ssh-authorized-keys-file ~/.ssh/id_rsa.pub; done)
+computeData=$(for i in `seq 1 $CNODES`; do oci compute instance launch --region $region --availability-domain "$AD" -c $C --shape "BM.Standard1.36" --display-name "hpc_cn_$i-$PRE" --image-id $OS --subnet-id $S --assign-public-ip true  --user-data-file hn_configure.sh --ssh-authorized-keys-file ~/.ssh/id_rsa.pub; done)
 
-#--skip-source-dest-check true 
+#--skip-source-dest-check true
 
 #LIST IP's
 echo
@@ -52,12 +61,13 @@ masterIP=$(oci compute instance list-vnics --region $region --instance-id $maste
 
 for iid in `oci compute instance list --region $region -c $C | jq -r '.data[] | select(."lifecycle-state"=="RUNNING") | .id'`; do newip=`oci compute instance list-vnics --region $region --instance-id $iid | jq -r '.data[0] | ."display-name"+": "+."private-ip"+", "+."public-ip"'`; echo $iid, $newip; done
 
-scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa opc@$masterIP:~/.ssh/
+scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa $USER@$masterIP:~/.ssh/
 
 #CREATE REMOVE SCRIPT
 cat << EOF >> removeCluster-$PRE.sh
 #!/bin/bash
 export masterIP=$masterIP
+export USER=$USER
 export C=$1
 export PRE=$PRE
 export region=$region
