fc601f9e7024b29709243224a111a36de826d71c
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-05-24 05:48:28 -0400

    Update bm_configure.sh

--- scripts/bm_configure.sh
+++ scripts/bm_configure.sh
@@ -46,7 +46,7 @@ mkdir -p /home/$MYUSER/bin
 
 cat << EOF >> /home/$MYUSER/.bashrc
 export WCOLL=/home/$MYUSER/hostfile
-export PATH=/opt/intel/compilers_and_libraries_2018.1.163/linux/mpi/intel64/bin:$PATH
+export PATH=/opt/intel/compilers_and_libraries_2018.1.163/linux/mpi/intel64/bin:'$PATH'
 export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2018.1.163/linux/mpi
 export MPI_ROOT=/opt/intel/compilers_and_libraries_2018.1.163/linux/mpi
 export I_MPI_FABRICS=tcp
