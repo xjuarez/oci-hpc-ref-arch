c1abb90d34f694ded4146ba12dc9fe68ebc77f7b
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-10-06 18:42:47 +0900

    Create configure_grafana.sh

---
+++ scripts/configure_grafana.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SERVER=$1
+
+curl -u admin:admin -X POST http://localhost:3000/api/datasources/ \
+-H "Content-Type: application/json" \
+-d '{"name":"ganglia2","type":"grafana-simple-json-datasource","url":"http://$SERVER:9000","access":"Server","basicAuth":false}'
+
