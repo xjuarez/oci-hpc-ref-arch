8f8b3fce55ee3042f81ece77e23d98f6d6f7a8cd
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-10-06 18:44:06 +0900

    Update configure_grafana.sh

--- scripts/configure_grafana.sh
+++ scripts/configure_grafana.sh
@@ -6,3 +6,6 @@ curl -u admin:admin -X POST http://localhost:3000/api/datasources/ \
 -H "Content-Type: application/json" \
 -d '{"name":"ganglia2","type":"grafana-simple-json-datasource","url":"http://$SERVER:9000","access":"Server","basicAuth":false}'
 
+curl -u admin:admin -X POST http://localhost:3000/api/dashboards/db/ \
+-H "Content-Type: application/json" \
+-d "@inputfile.payload"
