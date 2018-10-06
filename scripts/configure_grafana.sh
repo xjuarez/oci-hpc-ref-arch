114a16d4925c750339e68740a1cdcdd377a08e30
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-10-06 19:20:54 +0900

    Update configure_grafana.sh

--- scripts/configure_grafana.sh
+++ scripts/configure_grafana.sh
@@ -4,8 +4,12 @@ SERVER=$1
 cd /root/oci-hpc-ref-arch/scripts/
 curl -u admin:admin -X POST http://localhost:3000/api/datasources/ \
 -H "Content-Type: application/json" \
--d '{"name":"ganglia2","type":"grafana-simple-json-datasource","url":"http://$SERVER:9000","access":"Server","basicAuth":false}'
+-d @- <<EOF
+{"name":"ganglia2","type":"grafana-simple-json-datasource","url":"http://$SERVER:9000","access":"Server","basicAuth":false}
+EOF
 
 curl -u admin:admin -X POST http://localhost:3000/api/dashboards/db/ \
 -H "Content-Type: application/json" \
 -d "@grafana_dashboard.in"
+
+
