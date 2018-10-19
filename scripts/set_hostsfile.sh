1214a022a0e8ffe9fb639c88e48e581be3fbee41
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-10-19 08:49:05 -0400

    Update set_hostsfile.sh

--- scripts/set_hostsfile.sh
+++ scripts/set_hostsfile.sh
@@ -6,4 +6,4 @@ runuser -l $MYUSER -c "pdsh -w ^/home/$MYUSER/hostfile hostname > /home/$MYUSER/
 cat /home/$MYUSER/hostnames >> /etc/hosts
 sed -i 's/: / /g' /etc/hosts
 cp /etc/hosts /mnt/blk/share/hosts
-runuser -l $MYUSER -c "pdsh -w ^/home/$MYUSER/hostfile sudo cp /mnt/blk/share/hosts /etc/hosts
+runuser -l $MYUSER -c "pdsh -w ^/home/$MYUSER/hostfile sudo cp /mnt/blk/share/hosts /etc/hosts"
