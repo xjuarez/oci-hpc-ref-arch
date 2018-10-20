9288cf0a8b05a074a4c6188954709c4645f48ac4
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-10-20 13:41:26 -0400

    Update configure_grafana.sh

--- scripts/configure_grafana.sh
+++ scripts/configure_grafana.sh
@@ -5,11 +5,34 @@ cd /root/oci-hpc-ref-arch/scripts/
 curl -u admin:admin -X POST http://localhost:3000/api/datasources/ \
 -H "Content-Type: application/json" \
 -d @- <<EOF
-{"name":"ganglia","type":"grafana-simple-json-datasource","url":"http://$SERVER:9000","access":"Server","basicAuth":false}
+{
+"name":"ganglia",
+"type":"grafana-simple-json-datasource",
+"url":"http://$SERVER:9000",
+"access":"Server",
+"basicAuth":false
+}
 EOF
 
 curl -u admin:admin -X POST http://localhost:3000/api/dashboards/db/ \
 -H "Content-Type: application/json" \
 -d "@grafana_dashboard.in"
 
+curl -u admin:admin -X POST http://localhost:3000/api/datasources/ \
+-H "Content-Type: application/json" \
+-d @- <<EOF
+{
+"name":"foamData6",
+"type":"influxdb",
+"url":"http://localhost:8086",
+"access":"proxy",
+"basicAuth":false,
+"database":"foamrun",
+"user":"","password":""
+}
+EOF
+
+curl -u admin:admin -X POST http://localhost:3000/api/dashboards/db/ \
+-H "Content-Type: application/json" \
+-d "@grafana_dashboard2.in"
 
