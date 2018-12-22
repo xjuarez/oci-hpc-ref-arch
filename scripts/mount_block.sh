1622b43b94a9f9e0db76f6dac92aeda339ba6a68
tanewill <taylor.newill@oracle.com>
2018-12-21 21:16:28 -0500

    updates

--- scripts/mount_block.sh
+++ scripts/mount_block.sh
@@ -9,26 +9,25 @@ attach()
     iscsiadm -m node -o new -T $IQN -p $IPV4:3260
     iscsiadm -m node -o update -T $IQN -n node.startup -v automatic
     iscsiadm -m node -T $IQN -p $IPV4:3260 -l
-    yum -y -q install parted
 }
 
 mount()
 {
+    yum -y -q install parted
     sleep 30
     for i in `lsblk -d --noheadings | awk '{print $1}'`
     do 
         if [ $i = "sda" ]; then  break
         else
-            echo $i
             lsblk
             parted /dev/$i mklabel gpt
             parted -a opt /dev/$i mkpart primary ext4 0% 100%
             mkfs.ext4 -L datapartition /dev/$i\1
             mkdir -p /mnt/blk$i
-            mount -o defaults /dev/$i\1 /mnt/blk$i
             echo "/dev/$i"1" /mnt/blk$i ext4 defaults 0 2" | tee -a /etc/fstab
         fi
     done
+    mount -a
 }
 
 $operation
