6d8d44994a498e17448c6ed4b8f91513202c9c6a
tanewill <taylor.newill@oracle.com>
2019-01-09 09:00:33 -0800

    added profile

--- cli_deploy.sh
+++ cli_deploy.sh
@@ -16,10 +16,11 @@ set_variables()
   #export region=eu-frankfurt-1
   #export region=us-phoenix-1
   export region=uk-london-1
+  export P=YYZ
 
-  export AD=`oci iam availability-domain list -c $C --region $region --output table | grep 'AD-'$ad | awk '{ print $4 }'`
-  export OS=`oci compute image list -c $C --region $region --output table --query "data [*].{ImageName:\"display-name\", OCID:id}" | grep $IMAGE | awk '{ print $4 }'`
-  export INFO='--region '$region' --availability-domain '$AD' -c '$C
+  export AD=`oci iam availability-domain list -c $C --profile '$P' --region $region --output table | grep 'AD-'$ad | awk '{ print $4 }'`
+  export OS=`oci compute image list -c $C --profile '$P' --region $region --output table --query "data [*].{ImageName:\"display-name\", OCID:id}" | grep $IMAGE | awk '{ print $4 }'`
+  export INFO='--profile '$P' --region '$region' --availability-domain '$AD' -c '$C
 }
 
 create_key()
@@ -31,11 +32,11 @@ create_key()
 create_network()
 {
   #CREATE NETWORK
-  V=`oci network vcn create --region $region --cidr-block 10.0.$subnet.0/24 --compartment-id $C --display-name "hpc_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
-  NG=`oci network internet-gateway create --region $region -c $C --vcn-id $V --is-enabled TRUE --display-name "hpc_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
-  RT=`oci network route-table create --region $region -c $C --vcn-id $V --display-name "hpc_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
-  SL=`oci network security-list create --region $region -c $C --vcn-id $V --display-name "hpc_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
-  S=`oci network subnet create -c $C --vcn-id $V --region $region --availability-domain "$AD" --display-name "hpc_subnet-$PRE" --cidr-block "10.0.$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
+  V=`oci network vcn create --profile $P --region $region --cidr-block 10.0.$subnet.0/24 --compartment-id $C --display-name "hpc_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+  NG=`oci network internet-gateway create --profile $P --region $region -c $C --vcn-id $V --is-enabled TRUE --display-name "hpc_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+  RT=`oci network route-table create --profile $P --region $region -c $C --vcn-id $V --display-name "hpc_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
+  SL=`oci network security-list create --profile $P --region $region -c $C --vcn-id $V --display-name "hpc_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
+  S=`oci network subnet create -c $C --vcn-id $V --profile $P --region $region --availability-domain "$AD" --display-name "hpc_subnet-$PRE" --cidr-block "10.0.$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
 }
 
 create_fs()
@@ -43,8 +44,8 @@ create_fs()
   #CREATE FILE SYSTEM
   echo
   echo 'Creating File System'
-  #FSS=`oci fs file-system create --region $region --availability-domain "$AD" -c $C --display-name "HPC_File_System" --wait-for-state ACTIVE | jq -r '.data.id'`
-  #MT=`oci fs mount-target create --region $region --availability-domain "$AD" -c $C --subnet-id $S --display-name "mountTarget$g" --wait-for-state ACTIVE --ip-address 10.0.0.20 | jq -r '.data.id'`
+  #FSS=`oci fs file-system create --profile '$P' --region $region --availability-domain "$AD" -c $C --display-name "HPC_File_System" --wait-for-state ACTIVE | jq -r '.data.id'`
+  #MT=`oci fs mount-target create --profile '$P' --region $region --availability-domain "$AD" -c $C --subnet-id $S --display-name "mountTarget$g" --wait-for-state ACTIVE --ip-address 10.0.0.20 | jq -r '.data.id'`
 }
 
 create_headnode()
@@ -53,9 +54,9 @@ create_headnode()
   BLKSIZE_GB=`expr $BLKSIZE_TB \* 1024`
   BV=`oci bv volume create $INFO --display-name "hpc_block-$PRE" --size-in-gbs $BLKSIZE_GB --wait-for-state AVAILABLE | jq -r '.data.id'`
   masterID=`oci compute instance launch $INFO --shape "$SIZE" --display-name "hpc_"$PRE"_master" --image-id $OS --subnet-id $S --private-ip 10.0.$subnet.2 --wait-for-state RUNNING --user-data-file scripts/bm_configure.sh --ssh-authorized-keys-file $PRE.key.pub | jq -r '.data.id'`
-  attachID=`oci compute volume-attachment attach --region $region --instance-id $masterID --type iscsi --volume-id $BV --wait-for-state ATTACHED | jq -r '.data.id'`
-  attachIQN=`oci compute volume-attachment get --volume-attachment-id $attachID --region $region | jq -r .data.iqn`
-  attachIPV4=`oci compute volume-attachment get --volume-attachment-id $attachID --region $region | jq -r .data.ipv4`
+  attachID=`oci compute volume-attachment attach --profile $P --region $region --instance-id $masterID --type iscsi --volume-id $BV --wait-for-state ATTACHED | jq -r '.data.id'`
+  attachIQN=`oci compute volume-attachment get --volume-attachment-id $attachID --profile $P --region $region | jq -r .data.iqn`
+  attachIPV4=`oci compute volume-attachment get --volume-attachment-id $attachID --profile $P --region $region | jq -r .data.ipv4`
 }
 
 create_compute()
@@ -69,10 +70,10 @@ create_compute()
   echo 'Created Headnode and Compute Nodes'
   echo 'Waiting six minutes for init scripts to complete from: '`date +%T' '%D`
   sleep 360
-  masterIP=$(oci compute instance list-vnics --region $region --instance-id $masterID | jq -r '.data[]."public-ip"')
-  masterPRVIP=$(oci compute instance list-vnics --region $region --instance-id $masterID | jq -r '.data[]."private-ip"')
+  masterIP=$(oci compute instance list-vnics --profile $P --region $region --instance-id $masterID | jq -r '.data[]."public-ip"')
+  masterPRVIP=$(oci compute instance list-vnics --profile $P --region $region --instance-id $masterID | jq -r '.data[]."private-ip"')
 }
-#for iid in `oci compute instance list --region $region -c $C | jq -r '.data[] | select(."lifecycle-state"=="RUNNING") | .id'`; do newip=`oci compute instance list-vnics --region $region --instance-id $iid | jq -r '.data[0] | ."display-name"+": "+."private-ip"+", "+."public-ip"'`; echo $iid, $newip; done
+#for iid in `oci compute instance list --profile '$P' --region $region -c $C | jq -r '.data[] | select(."lifecycle-state"=="RUNNING") | .id'`; do newip=`oci compute instance list-vnics --profile '$P' --region $region --instance-id $iid | jq -r '.data[0] | ."display-name"+": "+."private-ip"+", "+."public-ip"'`; echo $iid, $newip; done
 
 configure_headnode()
 {
@@ -147,28 +148,29 @@ export SL=$SL
 export S=$S
 export BV=$BV
 export masterID=$masterID
+export P=$P
 
 
 #DELETE INSTANCES
 echo Removing: Head Node
-oci compute instance terminate --region $region --instance-id $masterID --force
+oci compute instance terminate --profile $P --region $region --instance-id $masterID --force
 
 EOF
 cat << "EOF" >> removeCluster-$PRE.sh
 echo Removing: Compute Nodes
-for instanceid in $(oci compute instance list --region $region -c $C | jq -r '.data[] | select(."display-name" | contains ("'$PRE'")) | .id'); do oci compute instance terminate --region $region --instance-id $instanceid --force; done
+for instanceid in $(oci compute instance list --profile $P --region $region -c $C | jq -r '.data[] | select(."display-name" | contains ("'$PRE'")) | .id'); do oci compute instance terminate --profile '$P' --region $region --instance-id $instanceid --force; done
 sleep 60
 echo Removing: Subnet, Route Table, Security List, Gateway, and VCN
-oci network subnet delete --region $region --subnet-id $S --force
+oci network subnet delete --profile $P --region $region --subnet-id $S --force
 sleep 10
-oci network route-table delete --region $region --rt-id $RT --force
+oci network route-table delete --profile $P --region $region --rt-id $RT --force
 sleep 10
-oci network security-list delete --region $region --security-list-id $SL --force
+oci network security-list delete --profile $P --region $region --security-list-id $SL --force
 sleep 10
-oci network internet-gateway delete --region $region --ig-id $NG --force
+oci network internet-gateway delete --profile $P --region $region --ig-id $NG --force
 sleep 10
-oci network vcn delete --region $region --vcn-id $V --force
-oci bv volume delete --region $region --volume-id $BV --force
+oci network vcn delete --profile $P --region $region --vcn-id $V --force
+oci bv volume delete --profile $P --region $region --volume-id $BV --force
 mv removeCluster-$PRE.sh .removeCluster-$PRE.sh
 mv $PRE.key .$PRE.key
 mv $PRE.key.pub .$PRE.key.pub
